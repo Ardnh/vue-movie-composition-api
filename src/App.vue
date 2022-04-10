80c6afbd9df7a38502954acf49abce75f02eb4a2
Ardan Hilal <ardanhilal@pop-os.localdomain>
2022-04-10 16:33:17 +0700

    Add Pinia, Vue router and Tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Viteeeeheheheee!!!" />
+  <p class="text-3xl">Hello world</p>
 </template>
+
+<script setup>
+
+
+
+</script>
 
 <style>
 #app {
